dfdd92b343f7e00d4eeb2c700b673f2027a6abb6
Catherine Karra and kate hall <catherine@>
2016-03-24 09:39:09 -0700

    add branching logic statements and conditions

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-  var age = parseInt(prompt("How old are you?"));
+  var height = parseInt(prompt("What is your height in feet?"));
 
-  if (age > 21) {
-    $('#drinks').show();
-  } else if (age === 21) {
-    alert("Now don't go crazy!");
-    $('#drinks').show();
+  if (height >= 7) {
+    alert("You're too tall for our rides. So sorry.");
+    $('#tootall').show();
+  } else if (height > 4) {
+    $('#scaryrides').show();
   } else {
-    $('#under-21').show();
+    $('#notscaryrides').show();
   }
 });
